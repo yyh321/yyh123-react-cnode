b84dd9946dae184e6da6957e6621e026c68858c0
yyh123 <yunhaoyu@gmail.com>
2019-01-11 17:37:27 +0800

    add mobx

--- client/views/App.jsx
+++ client/views/App.jsx
@@ -9,13 +9,12 @@ export default class App extends React.Component {
 
   render() {
     return [
-      <div>
+      <div key="banner">
         <Link to="/"> 首页 </Link>
-      </div>,
-      <div>
+        <br />
         <Link to="/detail"> 详情页 </Link>
       </div>,
-      <Routes />,
+      <Routes key="routes" />,
     ]
   }
 }
